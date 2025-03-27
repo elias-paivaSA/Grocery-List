2e8836e6403664cb3acb2a72de8c59e1520c6b64
elias-paivaSA <eliaspsa13@gmail.com>
2025-03-27 12:18:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-const inputValue = document.getElementById("grocery");
+const input = document.getElementById("grocery");
 const submitBtn = document.getElementById("btn");
+const main = document.getElementById("main");
+const clearButton = document.getElementById("clear");
 
-submitBtn.addEventListener("click", () => {
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const inputValue = input.value;
+    const newP = document.createElement("p");
+    const textValue = newP.innerHTML = inputValue;
+    main.appendChild(newP);
     
-})
+});
+
+clearButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    main.remove(); 
+});
